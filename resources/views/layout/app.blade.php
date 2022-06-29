8d9ba733604ac0066624903e33ab3262462f9b2b
gurpreetkait <100063447+GurpreetkaitCode@users.noreply.github.com>
2022-06-30 02:11:52 +0530

    code

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+@include('layout.front-includes.head')
+<html>
+    <body>
+        @yield('content')
+        @yield('scripts')
+        @include('layout.front-includes.footer')
+    </body>
+</html>
